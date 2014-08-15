5d3ffab27fbb20d5e846a6a3a937df9391dd1b09
Claus Guttesen <kometen@gmail.com>
2014-08-15 09:50:59 +0200

    Added requirements
    To model and view.
    http://stackoverflow.com/questions/13541983/html5-required-validation-in
    -ruby-on-rails-forms

--- Gemfile
+++ Gemfile
@@ -38,3 +38,5 @@ gem 'spring',        group: :development
 # Use debugger
 # gem 'debugger', group: [:development, :test]
 
+# https://github.com/amatsuda/html5_validators
+gem 'html5_validators'
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ActiveRecord::Base
+	include ActiveModel::Validations
+	validates_presence_of :text
+	validates_presence_of :title
+	validates_length_of :text, :maximum => 20
+	validates_length_of :title, :maximum => 40
 end
