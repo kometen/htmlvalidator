ce9aa00144a8aabb40f58dde64ba2a5bc3b0a870
Claus Guttesen <kometen@gmail.com>
2014-08-15 10:32:49 +0200

    Maxlength in textarea differs in model and view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
 	include ActiveModel::Validations
-	validates_presence_of :text
 	validates_presence_of :title
-	validates_length_of :text, :maximum => 20
-	validates_length_of :title, :maximum => 40
+	validates_presence_of :text
+	validates_length_of :title, :maximum => 10
+	validates_length_of :text, :maximum => 42
 end
